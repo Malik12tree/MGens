b5d6038f6345fcf30647f280a99cbdaa19f10057
Malik12tree <82341209+Malik12tree@users.noreply.github.com>
2021-09-16 06:41:15 +0300

    updated homepage

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function discord() {
     window.open('https://discord.gg/N5R8Gg7N7r','_blank');
 }
+function p() {
+    location.href = "pages/datapack_gens/particle-draw/particle-draw.html";
+}
+function t() {
+    location.href = "pages/datapack_gens/text-build/text-build.html";
+}
