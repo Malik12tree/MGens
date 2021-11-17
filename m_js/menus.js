31a59b825fdbcb17aa773f1926e34c9523f4b540
Malik12tree <82341209+Malik12tree@users.noreply.github.com>
2021-11-17 21:28:29 +0200

    better menus

---
+++ m_js/menus.js
@@ -0,0 +1,10 @@
+document.getElementsByClassName('dp-gens')[0].innerHTML = 
+`                
+    <ul>
+        <li><a href="${prefix}particle-draw/particle-draw.html">Particle Draw</a></li>
+        <li><a href="${prefix}text-build/text-build.html">Text-TypeWriter-Build</a></li>
+        <li><a href="${prefix}animated-build/animated-build.html">Animated Build</a></li>
+        <li><a href="${prefix}vox-particles/vox-particles.html">Vox To Particles</a></li>
+        <li><a href="${prefix}midi-to-noteblocks/midi-to-noteblocks.html">Midi To Noteblocks Notes</a></li>
+    </ul>
+`
